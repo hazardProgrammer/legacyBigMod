4cf35b6404679c955f3a2a5b9e97f745c3743cd8
hazardProgrammer <mlg.allen01@gmail.com>
2017-04-30 18:03:40 -0400

    Update bigMod.js

--- bigMod.js
+++ bigMod.js
@@ -5,7 +5,7 @@ name:'Big mod',
 author:'Hazard',
 desc:'A BIG mod.',
 engineVersion:1,
-manifest:'theModManifest.js',
+manifest:'BigManifest.js',
 requires:['Default dataset*'],
 sheets:{'styleSheet':'http:i.imgur.com/owlnODu.png'},//temporary stylesheet
 func:function()
